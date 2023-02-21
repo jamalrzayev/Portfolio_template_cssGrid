861471489572ae7b7ad19ce85fc323d1254d53f8
Jamal <jamal.rzayev@gmail.com>
2023-02-22 00:18:16 +0400

    first_screen_done

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      overlay = document.querySelector('.menu__overlay'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
